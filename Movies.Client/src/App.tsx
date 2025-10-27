faebe4086ee21b9ce39199c37c8df45b31a3ebcb
Daniel Sandoval <e.sandoval.daniel@gmail.com>
2025-10-27 11:47:59 -0600

    Completed activity 04: Conditional rendering

--- Movies.Client/src/App.tsx
+++ Movies.Client/src/App.tsx
@@ -3,12 +3,18 @@ import Content from "./Content"
 import Footer from "./Footer"
 
 function App() {
+const movies: any[] = [
+  {
+      title: "28 Years Later",
+      text: "28 Years Later is a 2025 post-apocalyptic horror film produced and directed by Danny Boyle and written by Alex Garland. The third in the 28 Days Later film series, it stars Jodie Comer, Aaron Taylor-Johnson, Jack O'Connell, Alfie Williams and Ralph Fiennes."
+  }
+];
 
   return (
     <>
       <div>
         <Header titlePage="Movie"/>
-        <Content title="28 Years Later" text="28 Years Later is a 2025 post-apocalyptic horror film produced and directed by Danny Boyle and written by Alex Garland. The third in the 28 Days Later film series, it stars Jodie Comer, Aaron Taylor-Johnson, Jack O'Connell, Alfie Williams and Ralph Fiennes."/>
+        <Content movieCatalog={movies}/>
         <Footer datetime={new Date().toLocaleDateString()} title="Movie App"/>
       </div>
     </>
